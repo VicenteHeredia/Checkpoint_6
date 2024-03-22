edad6b23a30aa3feef49ba4cf7943a74693c67a3
VicenteHeredia <152275830+VicenteHeredia@users.noreply.github.com>
2024-03-22 12:45:53 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+# Cree una clase de Python llamada Usuario que use el método init
+# y cree un nombre de usuario y una contraseña.
+# Crea un objeto usando la clase.
+
+class Usuario:
+    def __init__(self, usuario, contraseña):
+        self.usuario = usuario
+        self.contraseña = contraseña
+    
+    def formatter(self):
+        return f"Para el usuario {self.usuario} la contraseña es: {self.contraseña}"
+    
+usuario_uno = Usuario("Pepe", "pepe0123")
+
+print(usuario_uno.formatter)
